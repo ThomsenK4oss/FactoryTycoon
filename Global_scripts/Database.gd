1852be713ba360009ff1fba467b2d01b9c7d5ee2
tobiasb123 <tobias.bastholm@gmail.com>
2025-07-08 14:49:18 +0200

    Updated Map System
    The map system got a HUUUUGE!!! upgrade!!!!

---
+++ Global_scripts/Database.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var current_save: Dictionary = {
+	"Gold" : 200,
+	
+	"BaseFloorCost" : 100,
+	"FloorPlacement" : {},
+}
+
+var Saves: Dictionary = {
+	1 : {
+		"Gold" : 0,
+		
+		"BaseFloorCost" : 100,
+		"FloorPlacement" : {},
+	},
+	
+	2 : {
+		"Gold" : 0,
+		
+		"BaseFloorCost" : 100,
+		"FloorPlacement" : {},
+	},
+	
+	3 : {
+		"Gold" : 0,
+		
+		"BaseFloorCost" : 100,
+		"FloorPlacement" : {},
+	}
+}
--- Scripts/Tiles/map_system.gd
+++ Scripts/Tiles/map_system.gd
@@ -2,14 +2,35 @@ extends Node3D
 
 @onready var grid_map: GridMap = $GridMap
 
+@export var highlight_mesh: Node3D
+
+@export var can_buy: Resource
+@export var cant_buy: Resource
+
 func _ready() -> void:
 	place_wall(find_wall_placement())
 
 func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		var world_pos = get_mouse_pos()
+		if world_pos:
+			var local_pos = grid_map.to_local(world_pos)
+			var map_pos = grid_map.local_to_map(local_pos)
+			if get_tile_data(world_pos) == 1:
+				highlight_wall(world_pos)
+			else:
+				highlight_mesh.visible = false
+		else:
+			highlight_mesh.visible = false
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var hit = get_mouse_pos()
-		if hit:
-			get_tile_data(hit)
+		var world_pos = get_mouse_pos()
+		if world_pos:
+			var local_pos = grid_map.to_local(world_pos)
+			var map_pos = grid_map.local_to_map(local_pos)
+			if get_tile_data(world_pos) == 1:
+				if Database.current_save["Gold"] >= Database.current_save["BaseFloorCost"]:
+					place_floor(map_pos)
+					Database.current_save["Gold"] -= Database.current_save["BaseFloorCost"]
 
 func get_mouse_pos():
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -29,30 +50,56 @@ func get_mouse_pos():
 		return mouse_position_3D
 	return null
 
-func get_tile_data(world_pos: Vector3):
+func highlight_wall(world_pos):
+	var local_pos = grid_map.to_local(world_pos)
+	var map_pos = grid_map.local_to_map(local_pos)
+	var global_pos = grid_map.to_global(grid_map.map_to_local(map_pos))
+	
+	highlight_mesh.global_position = Vector3(global_pos.x, 3.25, global_pos.z)
+	
+	highlight_mesh.visible = true
+	
+	if Database.current_save["Gold"] >= Database.current_save["BaseFloorCost"]:
+		highlight_mesh.get_child(0).material_override = can_buy
+	else:
+		highlight_mesh.get_child(0).material_override = cant_buy
+
+func get_tile_data(world_pos: Vector3) -> int:
 	if world_pos:
 		var local_pos = grid_map.to_local(world_pos)
-		var map_pos = grid_map.local_to_map(local_pos) # Vector3i grid coordinate
+		var map_pos = grid_map.local_to_map(local_pos)
 
-		print("World Pos:", world_pos)
-		print("Local Pos:", local_pos)
-		print("Grid Pos (Vector3i):", map_pos)
+		#print("World Pos:", world_pos)
+		#print("Local Pos:", local_pos)
+		#print("Grid Pos (Vector3i):", map_pos)
 
 		var cell_id = grid_map.get_cell_item(map_pos)
 
 		if cell_id != -1:
-			print("✅ Found cell ID:", cell_id)
+			#print("✅ Found cell ID:", cell_id)
+			return cell_id
 		else:
-			print("❌ No cell data at", map_pos)
+			#print("❌ No cell data at", map_pos)
+			return -1
+	return -1
+
+func place_floor(map_pos):
+	grid_map.set_cell_item(map_pos, 0)
+	Database.current_save["FloorPlacement"][Database.current_save["FloorPlacement"].size() + 1] = map_pos
+	print(Database.current_save)
+	reset_walls()
+	place_wall(find_wall_placement())
+
+func reset_walls():
+	for cell in grid_map.get_used_cells():
+		var cell_id = grid_map.get_cell_item(cell)
+		if cell_id == 1:
+			grid_map.set_cell_item(cell, -1)
 
 func place_wall(wall_positions: Array):
 	for wall_position in wall_positions:
 		var wall_id = 1
 		grid_map.set_cell_item(wall_position, wall_id)
-		#var marker = MeshInstance3D.new()
-		#marker.mesh = BoxMesh.new()
-		#marker.transform.origin = wall_position
-		#add_child(marker)
 
 func find_wall_placement() -> Array:
 	var wall_positions: Array[Vector3i]
